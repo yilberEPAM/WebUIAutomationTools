4a5ae9162ee6370d6fbee9c2b188817bf95bf031
Yilber Calderon Gonzalez <yilber.calderongonzalez@ukg.com>
2025-10-19 21:57:03 -0500

    fix dependiencies

---
+++ CrossBrowser/CrossBrowser/Utilities/BrowserConfig.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Edge;
+using System;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace CrossBrowser.Utilities
+{
+    public class BrowserConfig
+    {
+        public static dynamic Config { get; private set; }
+
+        static BrowserConfig()
+        {
+            string configPath = Path.Combine(Directory.GetCurrentDirectory(), "Configuration", "config.json");
+            Config = JsonConvert.DeserializeObject(File.ReadAllText(configPath));
+        }
+
+        public static IWebDriver GetWebDriver(string browser)
+        {
+            switch (browser.ToLower())
+            {
+                case "chrome":
+                    return new ChromeDriver();
+                case "firefox":
+                    return new FirefoxDriver();
+                case "edge":
+                    return new EdgeDriver();
+                default:
+                    throw new ArgumentException($"El navegador '{browser}' no está soportado.");
+            }
+        }
+    }
+}
---
+++ CrossBrowser/CrossBrowser/Utilities/TestBase.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+
+namespace CrossBrowser.Utilities
+{
+    public class TestBase
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            string browser = TestContext.Parameters.Get("browser", "chrome");
+            driver = BrowserConfig.GetWebDriver(browser);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds((int)BrowserConfig.Config["implicitWait"]);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
